9bdfa495c64a8b9f38be111635512660af09cc4a
Andrey Tereschenko <worfdworfd@gmail.com>
2022-04-13 16:49:02 +0800

    Вторая лабораторная работа

---
+++ LAB2/Program.cs
@@ -0,0 +1,16 @@
+﻿using MinDLL;
+using System.Reflection;
+Assembly asm = Assembly.LoadFrom("MinDLL.dll");
+Type? t = asm.GetType("Class1");
+if (t is not null)
+{
+    object? obj = Activator.CreateInstance(t);
+    MethodInfo? min = t.GetMethod("Min", BindingFlags.NonPublic | BindingFlags.Static);
+    object? result = min?.Invoke(obj, new object[] { 10 });
+    Console.WriteLine(result);
+}
+else
+{
+    Class1 minDLL = new Class1();
+    minDLL.Min(10);
+}
---
+++ MinDLL/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace MinDLL
+{
+    public class Class1
+    {
+        public void Min(int size)
+        {
+            int SIZE = size;
+            var nums = Enumerable.Range(1, SIZE).ToList();
+            int[] result = new int[SIZE];
+            Random rand = new Random();
+            for (int i = 0; i < SIZE; i++)
+            {
+                result[i] = rand.Next(100);
+            }
+            foreach (var i in result)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine($" Минимальный элемент равен: {result.Min()}");
+        }
+    }
+}
